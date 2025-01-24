a462f44e088ef7b2eccd7948cd7de6459ae7bca5
Mario Lopez <mario@techmunchies.net>
2025-01-24 18:44:18 -0500

    add staging packages

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -6,3 +6,14 @@
 # Update the container and install packages
 dnf update -y
 grep -v '^#' ./devbox.packages | xargs dnf install -y
+
+# ublue staging repo needed for brew, etc
+dnf -y copr enable ublue-os/staging
+
+# common packages installed to desktops
+grep -v '^#' ./devbox.staging-packages | xargs dnf install -y
+
+# only the packages above should come from testing
+dnf -y copr disable ublue-os/staging
+
+# TODO: install homebrew packages
