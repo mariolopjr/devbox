386e68e9ca8cbd017892b226e1bf1effa7fa9549
Mario Lopez <mario@techmunchies.net>
2025-01-24 19:49:58 -0500

    fix build error related to shims and missing pkgs

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-# Symlink distrobox shims
-./distrobox-shims.sh
-
 # Update the container and install packages
 apk update && apk upgrade
 grep -v '^#' ./devbox.packages | xargs apk add
+
+# Get Distrobox-host-exec and host-spawn
+# TODO: fix shellcheck warnings
+RUN git clone https://github.com/89luca89/distrobox.git --single-branch /tmp/distrobox &&
+	cp /tmp/distrobox/distrobox-host-exec /usr/bin/distrobox-host-exec &&
+	cp /tmp/distrobox/distrobox-export /usr/bin/distrobox-export &&
+	cp /tmp/distrobox/distrobox-init /usr/bin/entrypoint &&
+	wget https://github.com/1player/host-spawn/releases/download/$(cat /tmp/distrobox/distrobox-host-exec | grep host_spawn_version= | cut -d "\"" -f 2)/host-spawn-$(uname -m) -O /usr/bin/host-spawn &&
+	chmod +x /usr/bin/host-spawn &&
+	rm -drf /tmp/distrobox &&
+	ln -fs /bin/sh /usr/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
--- scripts/distrobox-shims.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
