89d5082625f37a4f234026129b1841854dfe5b59
Mario Lopez <mario@techmunchies.net>
2025-01-24 23:37:35 -0500

    had a run in a shell file smdh

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -6,7 +6,7 @@ grep -v '^#' ./devbox.packages | xargs apk add
 
 # Get Distrobox-host-exec and host-spawn
 # TODO: fix shellcheck warnings
-RUN git clone https://github.com/89luca89/distrobox.git --single-branch /tmp/distrobox &&
+git clone https://github.com/89luca89/distrobox.git --single-branch /tmp/distrobox &&
 	cp /tmp/distrobox/distrobox-host-exec /usr/bin/distrobox-host-exec &&
 	cp /tmp/distrobox/distrobox-export /usr/bin/distrobox-export &&
 	cp /tmp/distrobox/distrobox-init /usr/bin/entrypoint &&
