c2bddec5a5fccc2f67b390a04561a5151aae4c03
Mario Lopez <mario@techmunchies.net>
2025-01-24 19:34:29 -0500

    change base image to wolfi

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -4,16 +4,5 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
-dnf update -y
-grep -v '^#' ./devbox.packages | xargs dnf install -y
-
-# ublue staging repo needed for brew, etc
-dnf -y copr enable ublue-os/staging
-
-# common packages installed to desktops
-grep -v '^#' ./devbox.staging-packages | xargs dnf install -y
-
-# only the packages above should come from testing
-dnf -y copr disable ublue-os/staging
-
-# TODO: install homebrew packages
+apk update && apk upgrade
+grep -v '^#' ./devbox.packages | xargs apk add
