1e8055c4a865c227612347b7087862841bf2510d
Mario Lopez <mario@techmunchies.net>
2025-01-30 20:28:57 -0500

    fix cut?

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -9,7 +9,7 @@ git clone https://github.com/89luca89/distrobox.git --single-branch /tmp/distrob
 	cp /tmp/distrobox/distrobox-host-exec /usr/bin/distrobox-host-exec &&
 	cp /tmp/distrobox/distrobox-export /usr/bin/distrobox-export &&
 	cp /tmp/distrobox/distrobox-init /usr/bin/entrypoint &&
-	curl "https://github.com/1player/host-spawn/releases/download/$(grep 'host_spawn_version=' /tmp/distrobox/distrobox-host-exec | cut -d '\"' -f 2)/host-spawn-$(uname -m) -o /usr/bin/host-spawn" &&
+	curl "https://github.com/1player/host-spawn/releases/download/$(grep 'host_spawn_version=' /tmp/distrobox/distrobox-host-exec | cut -d '"' -f 2)/host-spawn-$(uname -m) -o /usr/bin/host-spawn" &&
 	chmod +x /usr/bin/host-spawn &&
 	rm -rf /tmp/distrobox &&
 	ln -fs /bin/sh /usr/bin/sh
