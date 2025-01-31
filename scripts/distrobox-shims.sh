484981c596d88db26f072e2cf4d63948705d7eb9
Mario Lopez <mario@techmunchies.net>
2025-01-30 20:18:49 -0500

    not needed anymore

--- scripts/distrobox-shims.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /usr/local/bin
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/systemctl
